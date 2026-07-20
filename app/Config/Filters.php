12c2d49b88d4db38a77109eb2c390a2d9eb919d9
Jean Pierre Randriamahefa <jeanpierrerandriamahefa@gmail.com>
2026-07-20 09:43:03 +0300

    init

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -3,29 +3,49 @@
 namespace Config;
 
 use CodeIgniter\Config\Filters as BaseFilters;
-use App\Filters\AuthFilter;
-use App\Filters\RoleFilter;
+use CodeIgniter\Filters\Cors;
+use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\ForceHTTPS;
+use CodeIgniter\Filters\Honeypot;
+use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\PageCache;
 use CodeIgniter\Filters\PerformanceMetrics;
+use CodeIgniter\Filters\SecureHeaders;
 
 class Filters extends BaseFilters
 {
     public array $aliases = [
-        'csrf'=> \CodeIgniter\Filters\CSRF::class,
-        'auth'=> AuthFilter::class,
-        'role' => RoleFilter::class,
-        'forcehttps' => ForceHTTPS::class,
-        'pagecache' => PageCache::class,
-        'performance' => PerformanceMetrics::class,
-        'toolbar' => DebugToolbar::class,
+        'csrf'          => CSRF::class,
+        'toolbar'       => DebugToolbar::class,
+        'honeypot'      => Honeypot::class,
+        'invalidchars'  => InvalidChars::class,
+        'secureheaders' => SecureHeaders::class,
+        'cors'          => Cors::class,
+        'forcehttps'    => ForceHTTPS::class,
+        'pagecache'     => PageCache::class,
+        'performance'   => PerformanceMetrics::class,
     ];
-    public array $globals = [
+
+    public array $required = [
         'before' => [
-            'csrf',
+            'forcehttps',
+            'pagecache',
+        ],
+        'after' => [
+            'pagecache',
+            'performance',
+            'toolbar',
         ],
     ];
+
+    public array $globals = [
+        'before' => [
+        ],
+        'after' => [],
+    ];
+
     public array $methods = [];
+
     public array $filters = [];
 }
