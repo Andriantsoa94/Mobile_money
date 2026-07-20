6818b58ced47840609ed9ae5647da12a88aab3bc
Jean Pierre Randriamahefa <jeanpierrerandriamahefa@gmail.com>
2026-07-20 10:29:33 +0300

    Creation des donnees test

---
+++ app/Database/Seeds/AdminUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run()
+    {
+        $now = date('Y-m-d H:i:s');
+
+
+        $this->db->table('role')->insertBatch([
+            ['type' => 'admin'],
+            ['type' => 'client'],
+        ]);
+
+        $this->db->table('user')->insertBatch([
+            ['nom' => 'Jean' , 'idrole' => 1],
+        ]);
+
+        $this->db->table('numero')->insertBatch([
+            ['numero' => 0330000000 , 'iduser' => 1],
+        ]);
+    }
+}
